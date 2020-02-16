82056d89846a9ad67d300e7cc3243a26ed2003fb
mementum <mementum@users.noreply.github.com>
2020-02-16 23:04:18 +0100

    Add SEED_XXX enum to decice which seed to calculate in exp smoothing

--- btalib/utils.py
+++ btalib/utils.py
@@ -4,4 +4,12 @@
 # Copyright (C) 2020 Daniel Rodriguez
 # Use of this source code is governed by the MIT License
 ###############################################################################
-__all__ = []
+__all__ = [
+    'SEED_AVG', 'SEED_LAST', 'SEED_SUM',
+]
+
+
+SEED_AVG = 0
+SEED_LAST = 1
+SEED_SUM = 2
+
