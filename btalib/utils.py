7d090d3a67837f8d442faf36b546cda0e454f4a6
mementum <mementum@users.noreply.github.com>
2020-02-16 23:04:55 +0100

    Add period manipulation macro-like functions to cleanly support ta-lib compat

--- btalib/utils.py
+++ btalib/utils.py
@@ -6,6 +6,7 @@
 ###############################################################################
 __all__ = [
     'SEED_AVG', 'SEED_LAST', 'SEED_SUM',
+    '_INCPERIOD', '_DECPERIOD', '_MINIDX'
 ]
 
 
@@ -13,3 +14,39 @@ SEED_AVG = 0
 SEED_LAST = 1
 SEED_SUM = 2
 
+
+def _INCPERIOD(x, p=1):
+    '''
+    Forces an increase `p` in the minperiod of object `x`.
+
+    Example: `ta-lib` calculates `+DM` a period of 1 too early, but calculates
+    the depending `+DI` from the right starting point. Increasing the period,
+    without changing the underlying already calculated `+DM` values, allows the
+    `+DI` values to be right
+    '''
+    x._minperiod += p
+
+
+def _DECPERIOD(x, p=1):
+    '''
+    Forces an increase `p` in the minperiod of object `x`.
+
+    Example: `ta-lib` calculates `obv` already when the period is `1`,
+    discarding the needed "close" to "previous close" comparison. The only way
+    to take this into account is to decrease the delivery period of the
+    comparison by 1 to start the calculation before (and using a fixed
+    criterion as to what to do in the absence of a valid close to close
+    comparison)
+    '''
+    x._minperiod -= p
+
+
+def _MINIDX(x, p=0):
+    '''
+    Delivers the index to an array which corresponds to `_minperiod` offset by
+    `p`. This allow direct manipulation of single values in arrays like in the
+    `obv` scenario in which a seed value is needed for the 1st delivered value
+    (in `ta-lib` mode) because no `close` to `previous close` comparison is
+    possible.
+    '''
+    return x._minperiod - 1 + p
