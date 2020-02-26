a5cefc388f9cd7a9ae953ce87c8b6bf114bb8508
mementum <mementum@users.noreply.github.com>
2020-02-26 17:18:07 +0100

    Refactor crossover/up/down family, removing _type, simplifiyng formula and
    adding a _strict parameter to switch betwen 2 bar crossover (strict) and a
    crossover over multiple bars (non-strict)

--- btalib/indicators/crossover.py
+++ btalib/indicators/crossover.py
@@ -8,24 +8,36 @@ from . import Indicator
 
 
 class _crossbase(Indicator, inputs_override=True):
+    '''Base crossover class doing the calculations depending on the attribute
+    _updown, set by subclasses
+    '''
     _updown = 0  # -1 crossdown, 0 both, 1 crossup
 
     inputs = 'crosser', 'crossed'  # overrides (see above) and add an input
-    prams = (
-        ('_type', float, 'final type of the output. ex<: int, bool, float',)
+    params = (
+        ('_strict', False,
+         'If `True`, Consider cross only with two consecutive bars. Else the '
+         'default method will take into account that a cross up/down can '
+         'happen along several bars with the extremes being above/below and '
+         'below/above respectively, and the middle bars having a zero '
+         'difference between the crosser and crossed inputs'),
     )
 
     def __init__(self):
-        nzd = (self.i0 - self.i1).shift(periods=1)
-        nzd = nzd.replace(to_replace=0.0, method='ffill')
+        i0, i1 = self.i0, self.i1
+
+        idiff = (i0 - i1).shift(periods=1)  # inputs (io crosser, i1 crossed)
+        if not self.p._strict:  # fill 0.0 intervening bars with previous vals
+            # replace consecutive zeros with the value before the first zero
+            # this accounts for crossovers which happen over several bars, with
+            # the middle bars having both inputs equal (0.0 diff between them)
+            idiff = idiff.replace(to_replace=0.0, method='ffill')
 
         if self._updown >= 0:  # cross upwards
-            nzd = nzd.clip(upper=0.0).astype(bool)  # below
-            self._cup = (nzd & (self.i0 > self.i1)).astype(self.p._type)
+            self._cup = (idiff < 0.0) & (i0 > i1)  # i0(-1) < i1(-1) & ...
 
         if self._updown <= 0:  # cross downwards
-            nzd = nzd.clip(lower=0.0).astype(bool)  # above
-            self._cdown = (nzd & (self.i0 < self.i1)).astype(self.p._type)
+            self._cdown = (idiff > 0.0) & (i0 < i1)    # i0(-1) > i1(-1) & ...
 
 
 class crossup(_crossbase):
@@ -46,7 +58,7 @@ class crossup(_crossbase):
     _updown = 1
 
     def __init__(self):
-        self.o.crossup = self._cup
+        self.o.crossup = 1.0 * self._cup
 
 
 class crossdown(_crossbase):
@@ -58,8 +70,6 @@ class crossdown(_crossbase):
     index (-1) of both the 1st and 2nd data
 
     Formula:
-      - crossup = data0(-1) < data1(-1) and data0 > data1
-      - crossdown = data0(-1) > data1(-1) and data0 < data1
       - crossdown = data0(-1) > data1(-1) and data0 < data1
     '''
     group = 'utils'
@@ -69,7 +79,7 @@ class crossdown(_crossbase):
     _updown = -1
 
     def __init__(self):
-        self.o.crossdown = self._cdown
+        self.o.crossdown = -1.0 * self._cdown
 
 
 class crossover(_crossbase):
